05a3820cefbf46a7c7649502556b9c410f4f391e
thomasgerritsen-code <thomasgerritsen@outlook.com>
2026-08-26 14:20:22 +0200

    Refresh PWA cache for latest AprilTag reference sheet

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lichtveld-qa-v4-auto-markers';
+const CACHE='lichtveld-qa-v5-apriltag-sheet';
 const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./referentieblad_auto.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
