7a3035394b2a51141a9b9b671667421ef7191832
thomasgerritsen-code <thomasgerritsen@outlook.com>
2026-08-26 13:54:18 +0200

    Refresh cache for G T A B direction update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='lichtveld-qa-v2-auto-markers';
-const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest'];
+const CACHE='lichtveld-qa-v3-G-T-A-B';
+const ASSETS=['./','./index.html','./style.css','./directions.js','./app.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
