053356a0ba5ae135b1621030c4ecea56f1d99e24
thomasgerritsen-code <thomasgerritsen@outlook.com>
2026-08-26 14:07:07 +0200

    Refresh PWA cache for automatic marker version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='lichtveld-qa-v3-G-T-A-B';
-const ASSETS=['./','./index.html','./style.css','./directions.js','./app.js','./manifest.webmanifest'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+const CACHE='lichtveld-qa-v4-auto-markers';
+const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./referentieblad_auto.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request))));
