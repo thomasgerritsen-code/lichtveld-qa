0de6faf5df058d4d61582e4744f50a459f5be333
thomasgerritsen-code <thomasgerritsen@outlook.com>
2026-08-27 08:54:52 +0200

    Refresh cache for UMC layout v11

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='lichtveld-qa-v7-perspective-apriltag';
-const CORE=['./','./index.html','./style.css?v=7','./app_v7.js?v=7','./manifest.webmanifest?v=7'];
-const REFERENCE='referentieblad_apriltag36h11_v2.svg';
+const CACHE='lichtveld-qa-v11-umc-layout';
+const CORE=['./','./index.html','./style.css?v=11','./app_v7.js?v=11','./manifest.webmanifest?v=11','./umc-utrecht-banner.svg?v=1'];
+const REFERENCE='referentieblad_apriltag36h11_v3.svg';
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{const url=new URL(event.request.url);if(url.pathname.endsWith('/'+REFERENCE)){event.respondWith(fetch(event.request,{cache:'no-store'}));return;}event.respondWith(fetch(event.request,{cache:'no-cache'}).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request)))});
